cc5288c0ee6d9163e59f44acca6b7e24f6506558
Akhil Soni <akhilsoni2611@gmail.com>
2025-05-29 08:58:19 +0530

    Use of let const template literal

---
+++ ProfileSummary.js
@@ -0,0 +1,15 @@
+const name = "Akhil";
+const role = "Frontend Developer";
+const experience = "Student";
+
+let skills = ["C", "C++", "Java", "HTML", "CSS", "JS"];
+
+const profileSummary = `
+    Profile Summary 
+    ---------------
+    Name : ${name};
+    Role : ${this.role};
+    Experience : ${this.experience};
+    Skills : ${this.skills};
+`
+console.log(profileSummary);
